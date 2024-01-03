90f4a6dbfb3ead971e5c5e4b288f29e7c7d37581
AnjasFedo <fedoaf98@gmail.com>
2024-01-03 11:31:54 +0700

    Navbar Component

--- src/compoenents/Navbar.tsx
+++ src/compoenents/Navbar.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-
-import { FaEnvelope, FaLocationArrow, FaPhone } from "react-icons/fa";
-import Navlink from "./Navlink";
+import { FaBars, FaEnvelope, FaHome, FaLocationArrow, FaPhone } from "react-icons/fa";
+import { IoHome, IoPersonCircleOutline } from "react-icons/io5";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  const [navBarOpen, setNavBarOpen] = useState(false);
+  const [navBarOpen, setNavBarOpen] = useState(true);
+
   return (
     <nav className="sticky top-0 z-50">
       <div className="lg:block hidden bg-[#0ca39a] text-white py-2">
-        <div className="container mx-auto lg:flex hidden item-center justify-between px-3">
+        <div className="container mx-auto lg:flex items-center justify-between px-3">
           <span className="flex items-center gap-x-1">
             <FaLocationArrow />
             <p className="text-sm">Bengkulu, Indonesia</p>
@@ -25,25 +26,89 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      <div className="bg-white">
+        <div className="container mx-auto flex flex-wrap items-center justify-between px-3">
+          <div>
+            <ul className="lg:flex hidden items-center gap-x-4 list-none lg:ml-auto lg:gap-y-0 gap-y-8">
+              <li className="py-2 flex font-medium items-center leading-snug">
+                <button className="bg-[#0ca39a] rounded-full px-4 py-1 hover:bg-slate-950">
+                  Home
+                </button>
+              </li>
+              <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">About</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Listings</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Services</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Blogs</a>
+                </li>
+            </ul>
+          </div>
 
-      <div>
-        <ul>
-          <li>
-            <button>Home</button>
-          </li>
-          <li>
-            <Navlink to="/">About</Navlink>
-          </li>
-          <li>
-            <Navlink to="/">Listings</Navlink>
-          </li>
-          <li>
-            <Navlink to="/">Services</Navlink>
-          </li>
-          <li>
-            <Navlink to="/">Blocgs</Navlink>
-          </li>
-        </ul>
+          <div className="w-full lg:w-auto flex justify-between items-center">
+            <span className="flex items-center gap-x-2">
+              <button className="bg-[#0ca39a] text-white rounded-full w-14 h-14">
+                <IoHome className="w-1/2 h-1/2 m-auto" />
+              </button>
+              <p className="font-medium text-xl">Anjas</p>
+            </span>
+            <div className="flex items-center lg:hidden">
+              <button
+                className="cursor-pointer text-xl leading-none px-3 py-1 h-[3rem]"
+                onClick={() => setNavBarOpen(!navBarOpen)}
+              >
+                {navBarOpen ? <AiOutlineClose /> : <FaBars />}
+              </button>
+            </div>
+          </div>
+
+          <div
+            className={`lg:flex lg:items-center items-baseline pt-4 lg:h-auto h-screen ${
+              navBarOpen ? "flex" : "hidden"
+            }`}
+          >
+            <ul className="flex flex-col lg:flex-row items-center gap-x-4 list-none lg:ml-auto lg:gap-y-0 gap-y-8 ">
+              <div className="lg:hidden flex flex-col gap-y-8 items-center">
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <button className="bg-[#0ca39a] rounded-full px-4 py-1 ">
+                    Home
+                  </button>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">About</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Listings</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Services</a>
+                </li>
+                <li className="py-2 flex font-medium items-center leading-snug">
+                  <a href="#">Blogs</a>
+                </li>
+              </div>
+
+              <li className="py-2 flex font-medium items-center leading-snug">
+                <button className="py-4 flex items-center gap-2">
+                  <IoPersonCircleOutline />
+                  <p>Login/Register</p>
+                </button>
+              </li>
+
+              <li className="py-2 flex font-medium items-center leading-snug">
+                <button className="bg-[#0ca39a] rounded-full py-4 px-6 flex items-center gap-2">
+                  <FaHome className="text-xl" />
+                  <p>Add Listing</p>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
